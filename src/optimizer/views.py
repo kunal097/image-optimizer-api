67ea955791e940b17c6f9da724e60ec2c794ca6d
kunal097 <kunalsharma19997@gmail.com>
2018-09-03 18:45:43 +0530

    unlimited api calls

--- src/optimizer/views.py
+++ src/optimizer/views.py
@@ -3,50 +3,51 @@ from django.http import JsonResponse
 from .models import Image,Temp_User
 from.utils import compress , get_client_ip
 from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-from django.views.decorators.csrf import csrf_protect
 
-@csrf_protect
+
+@csrf_exempt
 def index(request):
     if request.method=='POST':
 
 
 
-        if request.user.is_anonymous:
-            client_ip = get_client_ip(request)
-            print('********')
-            # tu_obj = Temp_User.objects.get(ip_address = str(client_ip))
+        # if request.user.is_anonymous:
+        #     client_ip = get_client_ip(request)
+        #     print('********')
+        #     # tu_obj = Temp_User.objects.get(ip_address = str(client_ip))
 
 
-            try:
+        #     try:
 
 
-                tu_obj = Temp_User.objects.get(ip_address = str(client_ip))
-                print('#######')
+        #         tu_obj = Temp_User.objects.get(ip_address = str(client_ip))
+        #         print('#######')
 
 
-                created_date = str(tu_obj.time_stamp.date())
+        #         created_date = str(tu_obj.time_stamp.date())
 
-                today = str(datetime.today().date())
+        #         today = str(datetime.today().date())
 
-                if today in created_date:
-                    if tu_obj.count >=5 :
+        #         if today in created_date:
+        #             if tu_obj.count >=5 :
 
 
-                        return JsonResponse({'Message':'Limit exceed. Please Register to get access without limits'})
-                        # return redirect(RegisterView , permanent=True)
+        #                 return JsonResponse({'Message':'Limit exceed. Please Register to get access without limits'})
+        #                 # return redirect(RegisterView , permanent=True)
 
-                    else:
-                        tu_obj.count +=1
-                        tu_obj.save()
-                else:
-                    tu_obj.time_stamp=datetime.today()
-                    tu_obj.count=0
-                    tu_obj.save()
+        #             else:
+        #                 tu_obj.count +=1
+        #                 tu_obj.save()
+        #         else:
+        #             tu_obj.time_stamp=datetime.today()
+        #             tu_obj.count=0
+        #             tu_obj.save()
 
-            except:
-                Temp_User.objects.create(ip_address=client_ip)
+        #     except:
+        #         Temp_User.objects.create(ip_address=client_ip)
 
 
         img = Image()
